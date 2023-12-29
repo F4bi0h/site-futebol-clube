47457a6c9d687973fb4363e76b7a1ef0380a9b60
Fábio Henrique Nunes de Oliveira Souza <fabio@medware.local>
2023-12-29 16:54:27 -0300

    em desenvolvimento

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HcodeHeader/>
+    <HcodeSection/>
+    <HcodeFooter/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HcodeHeader from './components/HcodeHeader.vue';
+import HcodeFooter from './components/HcodeFooter.vue';
+import HcodeSection from './components/HcodeSection.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HcodeHeader,
+    HcodeFooter,
+    HcodeSection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
 </style>
---
+++ src/components/HcodeSectionNews.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <div class="container">
+            <HcodeSectionNewsIndividual img-name="news1.jpg" img-info="Notícia 1" news-title="Começam os treinos para a nova temporada" news-content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum assumenda exercitationem, aperiam ea, voluptas ipsa quae cumque nobis aliquam, quos doloremque harum voluptatum qui doloribus consequatur. Nobis velit excepturi aliquam!" news-date="26/12/2023"/>
+
+            <HcodeSectionNewsIndividual img-name="news2.jpg" img-info="Notícia 2" news-title="Jogo de quarta-feira termina empatado" news-content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum assumenda exercitationem, aperiam ea, voluptas ipsa quae cumque nobis aliquam, quos doloremque harum voluptatum qui doloribus consequatur. Nobis velit excepturi aliquam!" news-date="27/12/2023"/>
+
+            <HcodeSectionNewsIndividual img-name="news3.jpg" img-info="Notícia 3" news-title="A inauguração do novo estádio será na semana que vem" news-content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum assumenda exercitationem, aperiam ea, voluptas ipsa quae cumque nobis aliquam, quos doloremque harum voluptatum qui doloribus consequatur. Nobis velit excepturi aliquam!" news-date="30/12/2023"/>
+        </div>
+    </section>
+</template>
+<script>
+import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual.vue';
+export default {
+    components: {
+        HcodeSectionNewsIndividual
+    }
+}
+</script>
+<style scoped>
+section {
+    padding: 50px 0;
+    margin-top: 25px;
+    background-color: #F37520;
+}
+</style>
